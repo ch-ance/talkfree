8611f7e6354fb8f0a33f82a363eb3420ab323e74
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2021-09-07 16:30:25 -0500

    updates type defs

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,12 @@
 export interface IMessage {
+  id: string;
   text: string;
+  timestamp: number;
+  from: string;
+  to: string;
 }
 
 export interface IChannel {
+  id: string;
   name: string;
 }
