20bdca4102fc9aedb7000551cbf29efbe6747bdb
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2021-09-12 02:25:22 -0700

    creates a views directory, similar to components in our react app, with a Login component

---
+++ solidjs/src/views/Login/Login.tsx
@@ -0,0 +1,161 @@
+// const useStyles = makeStyles((theme) => ({
+//   container: {
+//     paddingLeft: 0,
+//     paddingRight: 0,
+//     marginLeft: 0,
+//     marginRight: 0,
+//     // TODO
+//     // change these max width values to something that makes sense.
+//     // for now I just want it to appear full width
+//     maxWidth: 4820,
+//     height: "100vh",
+//   },
+//   paper: {
+//     backgroundColor: theme.palette.primary.main,
+//     paddingTop: theme.spacing(8),
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//     width: "100%",
+//     height: "100%",
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main,
+//   },
+//   form: {
+//     width: "90%", // Fix IE 11 issue.
+//     marginTop: theme.spacing(1),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//   },
+// }));
+
+import { createSignal } from "solid-js";
+
+const Login = () => {
+  const [alias, setAlias] = createSignal("");
+  const [password, setPassword] = createSignal("");
+  return <h1>Hello</h1>;
+};
+
+// const Login = () => {
+//   const [aliasText, setAliasText] = useState("");
+//   const [passwordText, setPasswordText] = useState("");
+//   const classes = useStyles();
+
+//   const createNew = async (): Promise<boolean> => {
+//     return new Promise((resolve, reject) => {
+//       state.local.user().create(aliasText, passwordText, (ack) => {
+//         // @ts-ignore
+//         // const pub = ack.pub;
+//         // state.pub = pub;
+//         // localStorage.setItem("talkfreePubKey", pub);
+//         // setIsLoggedIn(true);
+//         // @ts-ignore
+//         if (ack.err) {
+//           // @ts-ignore
+//           alert(ack.err);
+//           reject(false);
+//         } else {
+//           resolve(true);
+//         }
+//       });
+//     });
+//   };
+
+//   const signIn = async () => {
+//     state.local
+//       .user()
+//       .recall({ sessionStorage: true })
+//       .auth(aliasText, passwordText, (ack) => {
+//         console.log("ack", ack);
+//         // @ts-ignore
+//         if (ack.err) {
+//           // @ts-ignore
+//           alert(ack.err);
+//           return;
+//         }
+//         // @ts-ignore
+//         const pub = ack.sea.pub;
+//         state.pub = pub;
+//         localStorage.setItem("talkfreePubKey", pub);
+//         setIsLoggedIn(true);
+//       });
+//   };
+
+//   return (
+//     <Container className={classes.container} component="main">
+//       <CssBaseline />
+//       <div className={classes.paper}>
+//         <Avatar className={classes.avatar}></Avatar>
+//         <Typography component="h1" variant="h5">
+//           Sign in
+//         </Typography>
+//         <form className={classes.form} noValidate>
+//           <TextField
+//             variant="outlined"
+//             margin="normal"
+//             required
+//             fullWidth
+//             id="alias"
+//             label="Alias"
+//             name="alias"
+//             autoFocus
+//             value={aliasText}
+//             onChange={(e) => setAliasText(e.target.value)}
+//           />
+//           <TextField
+//             variant="outlined"
+//             margin="normal"
+//             fullWidth
+//             name="password"
+//             label="Password"
+//             type="password"
+//             id="password"
+//             autoComplete="current-password"
+//             value={passwordText}
+//             onChange={(e) => setPasswordText(e.target.value)}
+//           />
+//           <FormControlLabel
+//             control={<Checkbox value="remember" color="primary" />}
+//             label="Remember me"
+//           />
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             className={classes.submit}
+//             onClick={(e) => {
+//               e.preventDefault();
+//               signIn();
+//             }}
+//           >
+//             Sign In
+//           </Button>
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             className={classes.submit}
+//             onClick={async (e) => {
+//               e.preventDefault();
+//               const newUser = await createNew();
+//               if (newUser) {
+//                 signIn();
+//               }
+//             }}
+//           >
+//             Create New Account
+//           </Button>
+//           <Grid container></Grid>
+//         </form>
+//       </div>
+//     </Container>
+//   );
+// };
+
+export default Login;
