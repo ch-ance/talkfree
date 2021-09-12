90d967a415df837483bac75330be1157bfc3ce78
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2021-09-12 02:26:02 -0700

    updates vite config to use hot loader

--- solidjs/vite.config.ts
+++ solidjs/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import shl from "solid-hot-loader";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), shl()],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
